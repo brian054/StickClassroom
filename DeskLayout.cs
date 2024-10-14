dac84bfff994fb99fbf7e3413839643cf909776a
brian054 <brian.rowell54@gmail.com>
2024-10-14 18:51:12 -0500

    Figured out Desk Layouts, implementing tomorrow.

---
+++ DeskLayout.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * So what I'm thinking is you pass in the Level value to the constructor (enum)
+ * then it builds the level based on that.
+ * 
+ * All the levels in a school type will be the same desk layout, the only difference is where
+ * the nerd is.
+ * 
+ * 
+ */
+
+namespace StickClassroom
+{
+    internal class DeskLayout
+    {
+    }
+}
