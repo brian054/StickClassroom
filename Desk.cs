e12e56426f0f0065018eeab2b39605838d27ba63
brian054 <brian.rowell54@gmail.com>
2024-10-31 20:53:02 -0500

    Added student rect. See what 10 minutes does lol baby steps are always gonna be better than no steps

--- Desk.cs
+++ Desk.cs
@@ -6,6 +6,7 @@ namespace StickClassroom
     internal class Desk
     {
         public Rectangle DeskRect { get; private set; }
+        public Rectangle StudentRect { get; private set; }
         private Texture2D texture;
 
         private int deskWidth = 80;
@@ -14,12 +15,14 @@ namespace StickClassroom
         public Desk(int x, int y, Texture2D texture) 
         {
             DeskRect = new Rectangle(x, y, deskWidth, deskHeight);
+            StudentRect = new Rectangle(x + 20, y + 20, 40, 40);
             this.texture = texture;
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(texture, DeskRect, Color.Brown);
+            spriteBatch.Draw(texture, StudentRect, Color.Brown);
         }
     }
 }
