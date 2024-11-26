31ce8f264e06f98ba6b56d38a550e13d9706df88
brian054 <brian.rowell54@gmail.com>
2024-11-25 19:33:35 -0600

    Gonna redesign this grid points thing tomorrow. Remember for now just show up every single day.

--- DeskLayout.cs
+++ DeskLayout.cs
@@ -43,13 +43,13 @@ namespace StickClassroom
             //    this.desks = new Desk[DeskRows, DeskCols];
             //    this.HighSchoolLayout(collidables);
             //}
-            else if (type.Equals("Elem"))
-            {
-                this.DeskRows = 4;
-                this.DeskCols = 4;
-                this.desks = new Desk[DeskRows, DeskCols];
-                this.PrimarySchoolLayout(collidables);
-            }
+            //else if (type.Equals("Elem"))
+            //{
+            //    this.DeskRows = 4;
+            //    this.DeskCols = 4;
+            //    this.desks = new Desk[DeskRows, DeskCols];
+            //    this.PrimarySchoolLayout(collidables);
+            //}
         }
 
         public void Draw(SpriteBatch spriteBatch)
@@ -62,7 +62,7 @@ namespace StickClassroom
                 }
             }
 
-            GridPoints.ForEach(entity => entity.Draw(spriteBatch, DeskTexture, Color.Orange, 5)); hmmm
+            GridPoints.ForEach(entity => entity.Draw(spriteBatch, DeskTexture, Color.Orange, 5)); //hmmm
         }
 
         private void HighSchoolLayout(List<Rectangle> collidables)
@@ -90,13 +90,36 @@ namespace StickClassroom
 
                     // --------------
                     // Grid Creation
+                    //
+                    // Problem: We can't do 00 and 01 coordinates 
+                    //
+                    //
+                    // MA = MiddleAisle
+                    // 
+                    //
                     // --------------
+
+                    /*
+                     * So I'm kinda rethinking this...maybe the desk class should generate the points around it. 
+                     * Yep doing that instead much much easier!
+                     * Remember you will rewrite all the mf time!! Shit is fun af
+                     *
+                     * Tuesday = rewrite and generate GridPoints in Desk class, 
+                     * Then we can access the desk's position in the level as "A1" so first column, first row or something and then to reference that gridpoint
+                     * it'd be "TL" or "BottomLeft (BL)" etc.
+                     */
+
+
                     // Create GridPoint instances around each desk
                     if (i == 0)
                     {
                         // Column 0, so that whole left side
-                        GridPoints.Add(new GridPoint(new Vector2(x - deskWidth / 2, y + deskHeight / 2), 1.0f, "move", "00"));
-                        GridPoints.Add(new GridPoint(new Vector2(x - deskWidth / 2, y + deskHeight + deskHeight + 10), 1.0f, "move", "00"));
+
+                        // To the left of each desk in first column (so labels = "00, 02, 04, 06, etc.)
+                        //GridPoints.Add(new GridPoint(new Vector2(x - deskWidth / 2, y + deskHeight / 2), 1.0f, "move", "00"));
+
+                        // Bottom left of each desk in column 0 (labels = "01, 03, etc")
+                        //GridPoints.Add(new GridPoint(new Vector2(x - deskWidth / 2, y + deskHeight + deskHeight + 10), 1.0f, "move", "00"));
                     }
                     else
                     {
@@ -104,7 +127,7 @@ namespace StickClassroom
                         GridPoints.Add(new GridPoint(new Vector2(x - deskWidth / 2 - 15, y + (deskHeight / 2)), 1.0f, "move", "00"));
 
                         // Mid right
-                        GridPoints.Add(new GridPoint(new Vector2(x - deskWidth / 2 - 15, y + deskHeight + deskHeight + 10), 1.0f, "move", "00"));
+                        //GridPoints.Add(new GridPoint(new Vector2(x - deskWidth / 2 - 15, y + deskHeight + deskHeight + 10), 1.0f, "move", "00"));
                     }
                     if (j == 0)
                     {
@@ -114,11 +137,43 @@ namespace StickClassroom
                     }
                     // Behind
                     GridPoints.Add(new GridPoint(new Vector2(x + deskWidth / 2, y + deskHeight + deskHeight + 10), 0.5f, "turn", "00"));
+
+                    // -----------------------------------------
+                    // Left side (if not the first column)
+                    //if (i > 0)
+                    //{
+                    //    Vector2 leftPosition = new Vector2(x - deskWidth / 2 - 15, y + deskHeight / 2);
+                    //    string positionString = $"{i}{j}_Left";
+                    //    GridPoints.Add(new GridPoint(leftPosition, 1.0f, "move", positionString));
+                    //}
+
+                    //// Right side (if not the last column)
+                    //if (i < DeskRows - 1)
+                    //{
+                    //    Vector2 rightPosition = new Vector2(x + deskWidth / 2 + 15, y + deskHeight / 2);
+                    //    string positionString = $"{i}{j}_Right";
+                    //    GridPoints.Add(new GridPoint(rightPosition, 1.0f, "move", positionString));
+                    //}
+
+                    //// Front (if not the first row)
+                    //if (j > 0)
+                    //{
+                    //    Vector2 frontPosition = new Vector2(x, y - deskHeight - 10);
+                    //    string positionString = $"{i}{j}_Front";
+                    //    GridPoints.Add(new GridPoint(frontPosition, 0.5f, "turn", positionString));
+                    //}
+
+                    //// Behind (always add)
+                    //Vector2 behindPosition = new Vector2(x, y + deskHeight + deskHeight + 10);
+                    //string positionStringBehind = $"{i}{j}_Behind";
+                    //GridPoints.Add(new GridPoint(behindPosition, 0.5f, "turn", positionStringBehind));
                 }
             }
         }
 
         // really just a four corners thing (BL = Teacher desk, all other corners = Student desks (4 students each)
+
+        // idea: those long tables like in middle school
 
         //private void MiddleSchoolLayout(List<Rectangle> collidables)
         //{
